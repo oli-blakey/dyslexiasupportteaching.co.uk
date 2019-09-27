913afcbc5778926574309b4c98a2c7aca0ff7c5e
oli <oliblakey@gmail.com>
2019-09-27 15:29:03 +0100

    new build

--- docs/js/leafMaps.js
+++ docs/js/leafMaps.js
@@ -4,6 +4,10 @@ var longitude = "-2.5980245";
 var zoom = 11;
 var mymap = L.map('mapLeaf').setView([latitude,longitude], zoom);
 
+mymap.scrollWheelZoom.disable();
+mymap.on('focus', () => { map.scrollWheelZoom.enable(); });
+mymap.on('blur', () => { map.scrollWheelZoom.disable(); });
+
 var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
